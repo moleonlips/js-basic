dbb54911b9ba5544cff43ec8fcdc289489a0a15e
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-01-07 14:41:42 +0700

    Comment syntax, type, userage and more...

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-/*
-Muc tieu khoa hoc:
--
--
--
-*/
+/**
+ * Comment theo khoi
+ * trong se profesional hon
+ * cac ki tu nam trong khoi nay se duoc trinh thong dich bo qua
+ * 
+ * Cac thc hien:
+ * bam 1 dau '/' va 2 ki tu '*'
+ */
+
+// comment theo 1 dong
+
+/**
+ * phim tat:
+ * window: ctrl + /
+ * mac: command + /
+ */
 
 var fullName = `LY THANH LONG`
 let filler = document.getElementById("filler")
