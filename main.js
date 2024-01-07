1e73027740c739afbb95b7dafee0c52dce273a7a
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-01-07 20:38:48 +0700

    built-in funtions

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 /**
- * Comment theo khoi
- * trong se profesional hon
- * cac ki tu nam trong khoi nay se duoc trinh thong dich bo qua
- * 
- * Cac thc hien:
- * bam 1 dau '/' va 2 ki tu '*'
+ * gioi thieu 1 so ham built-in co trong javascript
+ * 1. alert
+ * 2. console
+ * 3. confirm
+ * 4. promt
+ * 5. settimeout
+ * 6. setinterval
  */
 
-// comment theo 1 dong
+console.log(`this is a log`)
+console.log(123)
 
-/**
- * phim tat:
- * window: ctrl + /
- * mac: command + /
- */
+var fullName = `xin chao vietnam`
+console.log(fullName);
+console.warn(`log vua thoi`);
 
-var fullName = `LY THANH LONG`
-let filler = document.getElementById("filler")
+var isAdult = confirm(`Ban da tren 18 tuoi`);
+console.log(`- confirmed: `, isAdult);
 
-filler.innerText = fullName
+var whatYouEnter = prompt("Enter anything you are thinking about!")
+console.log(`- promted: `, whatYouEnter);
+
+setTimeout(() => {
+  console.log(`I waited 1s`);
+}, 1000);
+setTimeout(() => {
+  console.log(`I waited 2s`);
+}, 2000);
+setTimeout(() => {
+  console.log(`I waited 3s`);
+}, 3000);
+
+
+/// Send current time after each a second
+// setInterval(() => {
+//   console.log('counting up: ', new Date().getSeconds(), new Date().getMinutes(), new Date().getHours());
+// }, 1000);
