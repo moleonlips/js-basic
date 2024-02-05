18b1c0451b2cadd48bd1ecdd1893b7f9f1608450
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-05 15:31:35 +0700

    comparing operator part 2

--- main.js
+++ main.js
@@ -1,65 +1,11 @@
 /**
- * Kiểu dữ liệu
- * 1. Kiểu dữ liệu nguyên thuỷ - Primitive Data
- * - number
- * - string
- * - boolean
- * - undefined
- * - null
- * - symbol
- * 
- * 2. Dữ liệu phức tạp - Complex data
- * - function
- * - object
+ * Comparing operator
+ * ===
+ * !==
  */
 
-// number type
-var a = 0
-var a = 2
-var a = 3.1
+var a = 1
+var b = '1'
 
-// string type
-var fullname = 'TTNU'
-var empty = ''
-var something = 'i lvoe you'
-
-// boolean type
-var isOn = true
-var isOff = false
-
-// undefined type: is a variable that was declared but wasn't set any value
-var noone
-
-// null type
-var isNull = null
-
-// symbol type
-var id1 = Symbol('id')
-var id2 = Symbol('id')
-var compare = id1 == id2
-// console.log(`>> check symbol: ${compare}`);
-
-
-// function type
-var sayHello = function () {
-  console.log(`xin chao moi nguoi`);
-}
-
-
-// opject type
-var arr = [1, 2, 3, 5]
-var person = {
-  fName: 'cao van nam',
-  age: 12,
-  add: 'hungyen',
-  isMale: true,
-  activity: ['working', 'pooling', 'coffee', 'love'],
-  action: function () {
-    console.log('I did!');
-  }
-}
-
-person.action()
-
-console.log('>>> check type:', typeof person)
-console.log('dont forget, type of null is object:', null, 'is', typeof null)
+console.log(`${a} == ${b}:`, a == b);
+console.log(`${a} == ${b}:`, a === b);
