68b7d2b57f5b41dfd878d2a1ae49ba37eef0e26c
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-13 19:38:48 +0700

    a bit deep researching about function

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-// return trong ham - Js basic
+/**
+ * 1. khi function dat trung ten?
+ * 2. khi khai bao bien trong ham
+ * 3. dinh nghia ham trong ham
+ */
 
-function getSum(a, b) {
-  return a + b // co return -> tra ve ket qua duoc return
-  // khong co return: tra ve undefined
+// 1:
+function myFnt() {
+  console.log('1');
+}
+function myFnt() {
+  console.log('2');
+}
+function myFnt() {
+  console.log('3');
+}
+function myFnt() {
+  console.log('4');
+}
+// => se thuc thi fucntion duoc khai bao cuoi cung
+
+// 2:
+function testVarInFnt() {
+  var a = 1;
+  console.log('>>> check var: ', a)
+}
+// => Ket luan: co the tao bien trong ham
+
+// 3:
+function dadFnt(pa) {
+  function sonFnt(params) {
+    return params
+  }
+
+  sonFnt(pa)
 }
 
-var result = getSum(1, 2)
-
-console.log('>>> check result', result);
+console.log(dadFnt(123));
+// Concluding: able to defind a function inside another function
