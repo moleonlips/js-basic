77621fa09390f6a83bb17a49fec0efcf4ea6efd8
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-13 20:02:40 +0700

    list of type of function

--- main.js
+++ main.js
@@ -1,39 +1,50 @@
 /**
- * 1. khi function dat trung ten?
- * 2. khi khai bao bien trong ham
- * 3. dinh nghia ham trong ham
+ * List of type of functions
+ * 
+ * 1. Declarationn function
+ * - has Hoisting: yes
+ * 
+ * 2. Expression function
+ * - has Hoisting: no
+ * - co the dat ten hoac khong, (muc dich duy nhat cua ten ham chi de dev hieu duoc cong dung cua ham do, duoc su dung lam viec gi)
+ * 
+ * 3. Arrow function
+ * - duoc gioi thieu lan dau trong ES6
+ * - cho phep khai bao function voi cu phap ngan gon hon
  */
 
-// 1:
-function myFnt() {
-  console.log('1');
-}
-function myFnt() {
-  console.log('2');
-}
-function myFnt() {
-  console.log('3');
-}
-function myFnt() {
-  console.log('4');
-}
-// => se thuc thi fucntion duoc khai bao cuoi cung
-
-// 2:
-function testVarInFnt() {
-  var a = 1;
-  console.log('>>> check var: ', a)
-}
-// => Ket luan: co the tao bien trong ham
-
-// 3:
-function dadFnt(pa) {
-  function sonFnt(params) {
-    return params
-  }
-
-  sonFnt(pa)
+// 1. Declaration function - ham khai bao
+function showMessage() {
+  console.log('>> check declaration function');
 }
 
-console.log(dadFnt(123));
-// Concluding: able to defind a function inside another function
+// 2. Expression function -  ham bieu thuc
+var showMsg2 = function testName() {
+  console.log('>> check expression function');
+}
+
+// setTimeout(function testName() {
+
+// }, 2000);
+
+// var myObj = {
+//   myFnt: function testName() {
+
+//   }
+// }
+
+
+// 3. Arrow function
+var arf = () => {
+  console.log('>>> check arrow function!');
+}
+
+// arrow function return value by default
+var say = (saySomething) => saySomething
+
+
+// excuting
+showMessage()
+showMsg2()
+arf()
+console.log(say('>>> this is arrow function!'))
