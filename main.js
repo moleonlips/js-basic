9446754a0a50940af7cd84a45c0cfc4163608d6a
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-13 15:54:45 +0700

    return trong function

--- main.js
+++ main.js
@@ -1,35 +1,10 @@
-/** THAM SO VA DOI SO TRONG HAM
- * 1. Tham so:
- * - Dinh nghia
- * - Kieu du lieu
- * - Tinh private - chi su dung duoc trong ham
- * - 1 tham so
- * - nhieu tham so
- * 
- * 2. Tham so (parameters):
- * - duoc xac dinh khi dinh nghia ham
- * - 1 tham so hoac nhieu tham so
- * 
- * 3. Doi so (arguments):
- * - duoc truyen khi thuc thi (excute) ham
- * - co the truyen 1 hoac nhieu doi so, tuy thuoc vao so luong tham so khi dinh nghia (declare) ham
- */
+// return trong ham - Js basic
 
-function writeLog(msg) {
-  console.log(msg)
+function getSum(a, b) {
+  return a + b // co return -> tra ve ket qua duoc return
+  // khong co return: tra ve undefined
 }
 
-writeLog('xin chao moi nguoi')
-// => 1 tham so
+var result = getSum(1, 2)
 
-function checkGender(hasPenis, hiforgirl, hiforboy) {
-  if (hasPenis == false) {
-    console.log(`this is a girl`, `and she says ${hiforgirl}`);
-  }
-  else {
-    console.log(`this is a boy`, `and she says ${hiforboy}`);
-  }
-}
-
-checkGender(true, 'hi mom', `hello mther fuker`)
-checkGender(false, 'hi mom', `hello mther fuker`)
+console.log('>>> check result', result);
