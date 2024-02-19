f1d4add10ab9f188a89ed2128abde64f05f5de71
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-19 20:08:58 +0700

    wrking with number type in JS

--- main.js
+++ main.js
@@ -1,62 +1,46 @@
 /**
- * LAM VIEC VOI CHUOI
- * keyword: js string method
+ * kieu so (number) trong js
+ * 
+ * 1. tao gia tri number
+ * - cac cach tao
+ * - dung tnao, tsao
+ * - kiem tra datatype
+ * 
+ * 2. lam viec voi number
+ * - tostring
+ * - tofixed
  */
 
-// 1. length
-var mystring = 'xin chao viet nam'
-console.log('>>> check length of string:', mystring.length);
 
-// 2. find index
-var string2 = 'this is this my string'
-console.log('>>> index of "this":', string2.indexOf('this'));
-console.log('>>> index of "camxamita":', string2.indexOf('camxamita'));
+// cach tao 1 number thong thuong
+var age = 18;
+var PI = 3.14;
+// tao number su dung tu khoa new
+var otherNumber = new Number(9)
 
-function searchString(inputString, keyword) {
-  return inputString.indexOf(keyword) !== -1 ? `"${keyword}" was find in "${inputString}" at ${inputString.indexOf(keyword)}` : `"${keyword}" wasn't find in "${inputString}"`
+console.log(typeof PI); // number
+console.log(typeof age); // number
+console.log(typeof otherNumber); // object -> unexpected
+
+
+var result = 20 / 'xin chao'
+
+console.log(result); // NaN
+
+var rounded = PI.toFixed()
+
+console.log(rounded);
+
+
+// kiem tra 1 so co phai kieu so hay khong
+function isNumber(value) {
+  return isNaN(value) ? false : typeof value != 'number' ? false : true
 }
 
-/// tim kiem tu khoa trong 1 chuoi dua vao index
-var mystr = 'this is my string'
-console.log(searchString(mystr, 'is'));
-console.log(searchString(mystr, 'us'));
-console.log(searchString(mystr, 'long'));
+var v1 = '90'
+var v2 = 12
+var v3 = NaN
 
-console.log('>>> search attribute:', mystr.search('my s'))
-
-/// cut string
-var fullname = 'LY THANH LONG'
-// expect: chi tra ve lastname
-function getLastName(fullname) {
-  let _fName = String(fullname)
-  let lastSpace = _fName.lastIndexOf(' ')
-  return _fName.substring(lastSpace + 1, _fName.length) // using substring
-}
-
-console.log('>>> get lastname:', getLastName(fullname));
-
-// 2nd solution
-var outputstring = fullname.slice(3)
-console.log('>>> after slice:', outputstring);
-/// truyen so am de cat nguoc tu cuoi chuoi, vidu:
-console.log('>>> cat nguoc:', fullname.slice(-4)) // 'LY THANH LONG' -> 'LONG'
-
-
-// replace
-var mstr = 'hello world, wellcome to my JS course, lesson 1 - what is JS'
-// without REGEX: only first searchvalue is replaced
-console.log('>>> without regex:', mstr.replace('JS', 'Javascript'))
-
-// within REGEX: replace all searchvalue present in input string
-console.log('>>> within regex:', mstr.replace(/JS/g, 'Javascript'));
-
-
-// trim: Removes the leading and trailing white space and line terminator characters from a string, eg:
-var testTrim = '   xin chao moi nguoi  '
-console.log('>>> before trim:', testTrim, testTrim.length);
-console.log('>>> after trim:', testTrim.trim(), testTrim.trim().length);
-
-
-// split: 
-var testSplit = 'Java - PHP - Ruby - C# - C++'
-console.log('>>> after split:', testSplit.split(' - '));
+console.log(isNumber(v1));
+console.log(isNumber(v2));
+console.log(isNumber(v3));
