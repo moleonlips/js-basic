a337b341e3c11de1c447f5ee755f15015a68ec17
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-20 15:39:37 +0700

    object constructor, deeper research about OOP develop

--- main.js
+++ main.js
@@ -1,50 +1,35 @@
-// object trong javascript
-var myInfo = {
-  name: 'thanh long',
-  age: 12,
-  address: 'ecopark',
-  myKey: NaN
-}
-
-myInfo['cm-hc-cc'] = '09054937858394'
-
-myInfo.name = 'other name'
-
-
-console.log(myInfo);
-/**{
- name: 'other name',
- age: 12,
- address: 'ecopark',
- 'cm-hc-cc': '09054937858394'
-} */
-
-var myKey = 'address'
-console.log(myInfo[myKey]) // = myInfo['address'] => ecopark
-
-// dinh nghia them 1 cap key-value trong object
-console.log('>>> before:', myInfo);
-var emailKey = 'email'
-myInfo = {
-  ...myInfo,
-  [emailKey]: 'tonthel@gmail.com'
-}
-console.log('>>> after:', myInfo);
-
-// xoa 1 cap key-value trong obj:
-console.log('------\nxoa 1 cap key - value trong obj');
-console.log(`before: `, myInfo);
-delete myInfo.myKey
-console.log(`after: `, myInfo);
-console.log('>>> myKey is deleted');
-
-// can us save a value as a function
-var testValueisAFunction = {
-  name: 'thanh long', // aka a Property (thuoc tinh)
-  getname: function () { // aka a Method (phuong thuc)
-    return this.name // meaning this object
+/// object constructor
+function User(firstName, lastName, avatar) {
+  this.first = firstName
+  this.last = lastName
+  this.ava = avatar
+  this.getFullName = function () {
+    return this.first + ' ' + this.last
   }
 }
-console.log('>>> check function:');
-console.log(testValueisAFunction.getname()); // 'thanh long'
+
+var tlong = new User('ly', 'thnh', 'dz.jpg')
+
+console.log(`>>> check tlong`);
+console.log(tlong);
+console.log(tlong.getFullName());
+
+// check 1 doi tuong co phai duoc xay dung 1 mot constructor hay khong:
+console.log('>> check is constructor:', tlong.constructor == User)
+
+/// cach khac de tao object constructor:
+var banThietKe = function (_material, _height, _width, _length, _nofloor) {
+  this.material = _material
+  this.height = _height
+  this.width = _width
+  this.length = _length
+  this.nofloor = _nofloor
+
+  this.getArea = function () {
+    return this.width * this.length
+  }
+}
+
+var firstHouse = new banThietKe('xi mang', 190, 10, 15, 4)
+console.log(`>> check first house:`, firstHouse.getArea());
 
