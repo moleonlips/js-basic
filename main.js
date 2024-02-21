47e6abe27803d28b3f79541141b95d2bbd7d745d
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-21 13:46:03 +0700

    object prototype and more about object constructor

--- main.js
+++ main.js
@@ -1,35 +1,65 @@
-/// object constructor
+// object prototype - basic
+/**
+ * 1. prototype la gi (~ la nguyen mau cua doi tuong)
+ * 2. su dung prototype khi nao?
+ */
+
 function User(firstName, lastName, avatar) {
-  this.first = firstName
-  this.last = lastName
-  this.ava = avatar
-  this.getFullName = function () {
-    return this.first + ' ' + this.last
+  this.firstName = firstName
+  this.lastName = lastName
+  this.avatar = avatar
+
+  this.getName = function () {
+    return `${this.firstName} ${this.lastName}`
   }
 }
 
-var tlong = new User('ly', 'thnh', 'dz.jpg')
-
-console.log(`>>> check tlong`);
-console.log(tlong);
-console.log(tlong.getFullName());
-
-// check 1 doi tuong co phai duoc xay dung 1 mot constructor hay khong:
-console.log('>> check is constructor:', tlong.constructor == User)
-
-/// cach khac de tao object constructor:
-var banThietKe = function (_material, _height, _width, _length, _nofloor) {
-  this.material = _material
-  this.height = _height
-  this.width = _width
-  this.length = _length
-  this.nofloor = _nofloor
-
-  this.getArea = function () {
-    return this.width * this.length
-  }
+// dinh nghia them 1 thuoc tinh ben ngoai ham tao
+User.prototype.className = 'gto'
+User.prototype.sayHi = function () {
+  return `hi my name is ${this.firstName} ${this.lastName}`
 }
 
-var firstHouse = new banThietKe('xi mang', 190, 10, 15, 4)
-console.log(`>> check first house:`, firstHouse.getArea());
+var u1 = new User(`thanh`, `long`, `avt.jpg`);
+var u2 = new User(`tun`, `pham`, `avt.jpg`);
+var u3 = new User()
 
+console.log(`>>> check u3`, u3); // check u3 before set value
+
+u3 = {
+  avatar: 'xinchao.png',
+  firstName: 'do',
+  lastName: 'hieu'
+}
+console.log(u3);
+
+u3.firstName = 'chiu'
+u3.lastName = 'van'
+u3.avatar = 'anonimous.webs'
+console.log(u3);
+
+console.log(u1.className);
+console.log(u2.sayHi());
+console.log('>>> check u3 after set value:', u3);
+console.log(u3 === u2); // true
+
+/**
+ * bai tap thuc hanh:
+ * tao 1 object constructor Student, voi cac propertis: firstName, lastName
+ * sau do dinh nghia them 1 phuong thuc getFullName tra ve fullname cua Student
+ */
+
+function Student(firstName, lastName) {
+  this.firstName = firstName,
+    this.lastName = lastName
+}
+
+Student.prototype.getFullName = function () {
+  return this.firstName + ' ' + this.lastName
+}
+
+// kiem tra ket qua:
+console.log(`Kiem tra ket qua:`);
+var stu1 = new Student('tran', 'uyen')
+console.log(`stu1:`, stu1);
+console.log(`getFullName method: `, stu1.getFullName());
