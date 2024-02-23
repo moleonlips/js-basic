fabf177af74bf4eb3e29c9245a2d683881f82032
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-23 16:24:15 +0700

    bo sung comment cho can than

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/**
+ * Luu y: Thu tu uu tien doi voi cac dieu kien re nhanh:
+ * - Cac dieu kien nao mang tinh dam bao cao hon (neu no xay ra thi cac dieu kien khac se xay ra) thi nen duoc uu tien kiem tra truoc.
+ * @param {int} a So tu nhien cho truoc
+ * @returns Tra ve kha nang chia het cua 'a' cho cac so 3, 5, 15 tuong ung voi cac gia tri 1, 2, 3
+ */
+
 function run(a) {
   if (a % 15 === 0) {
     return 3
