c9de5ab8e825e54f61d8d0abe9fca556cb079e05
Ly T. Long <thanhlong.dev.1810@gmail.com>
2024-02-23 16:17:36 +0700

    IF - ELSE practice with 3, 5, 15

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-// cau lenh re nhanh if-else
-
-var day = Math.ceil(Math.random() * 7)
-
-if (day === 1) {
-  console.log(`sunday the king plays`);
-} else if (day === 2) {
-  console.log(`hello monday`);
-} else if (day === 3) {
-  console.log(`tuesday`);
-} else if (day === 4) {
-  console.log(`wednesday`);
-} else if (day === 5) {
-  console.log(`thur day roi`);
-} else if (day === 6) {
-  console.log(`thank god its friday`);
-} else {
-  console.log(`saturday, let's play`);
+function run(a) {
+  if (a % 15 === 0) {
+    return 3
+  }
+  else if (a % 5 === 0) {
+    return 2
+  }
+  else if (a % 3 === 0) {
+    return 1
+  }
+  else {
+    return 0
+  }
 }
+
+// Kỳ vọng
+console.log(run(3)) // 1
+console.log(run(5)) // 2
+console.log(run(15)) // 3
